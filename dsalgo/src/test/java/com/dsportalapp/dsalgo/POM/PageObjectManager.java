04e97b2e3df967c1879e889d73ce6f70995cd275
pravinapatel <pravinaSDET153@gmail.com>
2024-03-28 21:33:23 -0400

    pravina fisrt commit

--- dsalgo/src/test/java/com/dsportalapp/dsalgo/POM/PageObjectManager.java
+++ dsalgo/src/test/java/com/dsportalapp/dsalgo/POM/PageObjectManager.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 public class PageObjectManager {
 	public WebDriver driver;
 	public HomePageObjects homepageobj;
+	public RegisterPageObjects registerobjects;
+	public QueuePageObjects queueobjects;
 
 	public PageObjectManager(WebDriver driver) {
 		super();
@@ -14,6 +16,16 @@ public class PageObjectManager {
 	public HomePageObjects getHomePageObjects() {
 		homepageobj = new HomePageObjects();
 		return homepageobj;
+		
+	}
+	public RegisterPageObjects getRegisterPageObjects() {
+		registerobjects = new RegisterPageObjects(driver);
+		return registerobjects;
+		
+	}
+	public QueuePageObjects getQueuePageObjects() {
+		queueobjects = new QueuePageObjects(driver);
+		return queueobjects;
 		
 	}
 	
--- dsalgo/src/test/java/com/dsportalapp/dsalgo/testRunner/testRunnerTestNG.java
+++ dsalgo/src/test/java/com/dsportalapp/dsalgo/testRunner/testRunnerTestNG.java
@@ -7,12 +7,12 @@ public class testRunnerTestNG {
 
 	@CucumberOptions(
 			features=
-					"src/test/resources/features/HomePage.feature",
+					"src/test/resources/features/RegistrationPage.feature",
 			glue="com.dsportalapp.dsalgo.stepDefinition",
 			monochrome = true,
-//			dryRun=true
+			dryRun=true
 //			plugin = {"pretty", "html:target/cucumber.html","json:target/cucmber.json",
-			plugin ={"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+			//plugin ={"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 			)
 	public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 	
