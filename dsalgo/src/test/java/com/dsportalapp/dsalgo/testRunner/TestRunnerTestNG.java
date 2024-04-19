600adf7f65f31eff0a56fbb57305b311e995707b
sudhal85 <lsudha.vel@gmail.com>
2024-04-19 10:44:47 -0400

    testrunner features

--- dsalgo/src/test/java/com/dsportalapp/dsalgo/testRunner/TestRunnerTestNG.java
+++ dsalgo/src/test/java/com/dsportalapp/dsalgo/testRunner/TestRunnerTestNG.java
@@ -11,7 +11,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 
 
-		features = "src/test/resources/features/RegistrationPage.feature",
+		features = "src/test/resources/features/",
 		glue = {"com.dsportalapp.dsalgo.stepDefinition","com.dsportalapp.dsalgo.hooks"},
 		monochrome = true,
 //		dryRun=true
