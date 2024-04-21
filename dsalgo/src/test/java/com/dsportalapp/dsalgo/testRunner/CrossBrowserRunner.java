be3d1ef10e29c8507bbffbfb468a1d52c909db32
sudhal85 <lsudha.vel@gmail.com>
2024-04-21 10:54:22 -0400

    crossbrowse test

--- dsalgo/src/test/java/com/dsportalapp/dsalgo/testRunner/CrossBrowserRunner.java
+++ dsalgo/src/test/java/com/dsportalapp/dsalgo/testRunner/CrossBrowserRunner.java
@@ -1,45 +1,34 @@
 package com.dsportalapp.dsalgo.testRunner;
 
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
-
 import com.dsportalapp.dsalgo.utilities.ConfigReader;
-import com.dsportalapp.dsalgo.webDriverManager.DriverManager;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-
 		features = "src/test/resources/features/HomePage.feature",
-
-
-		glue = {"com.dsportalapp.dsalgo.stepDefinition","com.dsportalapp.dsalgo.hooks"},
+		glue = { "com.dsportalapp.dsalgo.stepDefinition", "com.dsportalapp.dsalgo.hooks" },
 		monochrome = true,
+		plugin = { "pretty", "html:target/cucumber.html", "json:target/cucumber.json", "pretty",
+				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "pretty",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
 
-//		dryRun=true
-		plugin = {"pretty", "html:target/cucumber.html","json:target/cucumber.json",
-		       	  "pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-			      "pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
-		)
-public class CrossBrowserRunner extends AbstractTestNGCucumberTests{
+public class CrossBrowserRunner extends AbstractTestNGCucumberTests {
 	
 	String browser;
 	
 	@BeforeClass
 	@Parameters("browser")
-	public  void beforeClass(@Optional String browser) {
-		 this.browser = browser;
-		if(browser != null && !browser.equals("param-val-not-found")) {
+	public void beforeClass(@Optional String browser) {
+		this.browser = browser;
+		if (browser != null && !browser.equals("param-val-not-found")) {
 			ConfigReader.setProperty("browser", browser);
-
 		}
 	}
-	
+}
 
 
-	}
 
